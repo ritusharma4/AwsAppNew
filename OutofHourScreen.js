38d869b2a02e07ffdfdd0179ed7ecc23cb1201c1
ritusharma4 <ritusharmasvs@gmail.com>
2022-01-21 18:34:33 +0530

    OutofHours issue fixed

--- OutofHourScreen.js
+++ OutofHourScreen.js
@@ -40,73 +40,100 @@
  );
  
  export function OutofHourScreen({ navigation }) {
-     const [users, setUsers] = React.useState([]);
-     const itemListing = 
-     [
-               {
-       
-                   "Name": "Glasgow SE Foodbank",
-       
-                   "AddressInfo": "42 Inglefield St, Glasgow G42 7AT",
-       
-                   "Telephone": "01414 232 418"},
-              
-               {
-   
-               "Name": "Glasgow NE Foodbank",
-   
-               "AddressInfo": "142 Helenvale St, Glasgow G31 4NA",
-   
-               "Telephone": "07745 242 738"},
-               {     
-   
-               "Name": "Glasgow NW Foodbank - Trussel Trust",
-   
-               "AddressInfo": "Millbrix Ave, Glasgow G14 0EP",
-   
-               "Telephone": "07787 334 012"},
-               {     
-   
-               "Name": "The Trussel Trust Food Bank",
-   
-               "AddressInfo": "2 Kirkwood St, Rutherglen, Glasgow G73 2SL",
-   
-               "Telephone": "07393 737 030"
-               }
-           ];
+     const [category, setCategory] = React.useState([]);
+     const [subcategory, setSubCategory] = React.useState([]);
+      const itemListing = 
+      [{
+            "Categoryname": "Foodbanks",
+            "subcategory": [
+                {
+        
+                    "Name": "Glasgow SE Foodbank",
+        
+                    "AddressInfo": "42 Inglefield St, Glasgow G42 7AT",
+        
+                    "Telephone": "01414 232 418"},
+               
+                {
     
+                "Name": "Glasgow NE Foodbank",
+    
+                "AddressInfo": "142 Helenvale St, Glasgow G31 4NA",
+    
+                "Telephone": "07745 242 738"},
+                {     
+    
+                "Name": "Glasgow NW Foodbank - Trussel Trust",
+    
+                "AddressInfo": "Millbrix Ave, Glasgow G14 0EP",
+    
+                "Telephone": "07787 334 012"},
+                {     
+    
+                "Name": "The Trussel Trust Food Bank",
+    
+                "AddressInfo": "2 Kirkwood St, Rutherglen, Glasgow G73 2SL",
+    
+                "Telephone": "07393 737 030"
+                }
+            ]
+
+            },
+            {
+                "Categoryname": "Samaritans",
+                "subcategory": [{
+    
+                "Name": "The Trussel Trust Food Bank",
+    
+                "AddressInfo": "2 Kirkwood St, Rutherglen, Glasgow G73 2SL",
+    
+                "Telephone": "07393 737 030"},
+            ]
+
+}];
+     
+       
+    
+   
       const onPressBack = (event) => {
         navigation.navigate('Login');
     }
      useEffect(() => {
-         const onGetUserList = async () => {
-             firestore()
-                 .collection('User')
-                 .get()
-                 .then(querySnapshot => {
+         
                      const arrayUserList = [];
-                     querySnapshot.forEach(documentSnapshot => {
-                         console.log('User ID: ', documentSnapshot.id, documentSnapshot.data());
-                         arrayUserList.push(documentSnapshot.data());
+                     itemListing.forEach(documentSnapshot => {
+                         console.log('User ID: ', documentSnapshot);
+                         arrayUserList.push(documentSnapshot);
+                        
+                         const arrayUserList2 = [];
+
+                         arrayUserList.forEach(documentSnapshot2 => {
+                            console.log('Sub Category: ', documentSnapshot2.subcategory);
+                            arrayUserList2.push(documentSnapshot2.subcategory);
+   
+   
+                        });
+
+                         setSubCategory(arrayUserList2)
+                         console.log('Sub Category array: ',arrayUserList2);
+
                      });
- 
-                     setUsers(arrayUserList)
-                 });
-         }
- 
-         onGetUserList()
+                     setCategory(arrayUserList)
+                     console.log('Category array: ', arrayUserList);
+
+           
      }, [])
  
      const renderItem = ({ item }) => (
          <Item 
-         title={item.Name} subtitle={item.AddressInfo} telephone={item.Telephone} />
+         title={item.Name} subtitle={item.AddressInfo} telephone ={item.Telephone} />
      );
  
      const isDarkMode = useColorScheme() === 'dark';
      return (
          <SafeAreaView style={[styles.viewParent]}>
              <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-             <ScrollView>
+             <ScrollView style={{flex: 1}}>
 <View>
 
              <View style={styles.headerWhite}>
@@ -128,18 +155,46 @@
                     </View>
                     <View style={styles.viewTxt}>
                      <Text style={styles.txtTitle}>Out of Hours Support</Text>
-                     <Text style={styles.txtTitle2}>Foodbanks</Text>
+                     {/* <Text style={styles.txtTitle2}>Foodbanks</Text> */}
 
                      </View>
              <View>
+            <FlatList
+                            style={{ marginLeft: 10, marginRight: 10, }}
+                            // listkey={item => item.id}
+                            data={category}
+                            renderItem={({ item, index }) => (
+
+                                <View style={{ width: width, alignSelf: 'center', marginTop: 10, }}>
+                                        <Text style={styles.txtTitle2}>{item.Categoryname}</Text> 
                  <FlatList
-                 style={{width:width,backgroundColor:'#ebfdff'}}
-                     data={itemListing}
+                style={{width:width,backgroundColor:'#ebfdff'}}
+                     data={item.subcategory}
                      keyExtractor={item => `${item.Name}`}
-                     renderItem={renderItem}
-                     keyExtractor={item => item.id}
-                     renderS
+                    //  renderItem={renderItem}
+                    //  keyExtractor={item => item.id}
+                     
+                    renderItem={({ item, index }) => (
+                        <View style={styles.item}>
+                        {/* <Text style={styles.title}>{`Name: ${title}`}</Text> */}
+                        <Text style={styles.title}>{item.Name}</Text>
+               
+                        <View style={{flexDirection:'row',marginTop:10,alignItems: 'center'}}>
+                            <Image style={{width:25, height:25}}
+                            source={require("./assets/phone.png")} 
+                            ></Image>
+                            <Text style={{marginLeft:10,    fontSize: 16,}}>{item.Telephone}</Text>
+                        </View>
+                        <Text style={styles.subtitle}>{item.AddressInfo}</Text>
+                    </View>
+                        )}
+
                  />
+                                
+                              </View>
+                            )}
+
+                        /> 
              </View>
 
             
@@ -203,6 +258,7 @@
        marginTop:20,
    },
 
+
    textButton: {
     color: '#91bfb5',
     fontSize: 20,
@@ -220,7 +276,7 @@
 },
 txtTitle2: {
     fontSize: 18,
-    marginBottom: 20,
+    marginBottom: 10,
     color: 'white',
     textAlign: 'center',
     alignItems: 'center',
@@ -229,12 +285,13 @@ txtTitle2: {
 
 },
 txtTitle3: {
-    fontSize: 17,
+    fontSize: 18,
     marginBottom: 20,
     color: 'black',
     textAlign: 'center',
     alignItems: 'center',
-    letterSpacing:1
+    letterSpacing:1,
+    fontWeight: 'bold',
 
 },
 button: {
